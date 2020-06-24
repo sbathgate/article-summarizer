a62a0c77cce4bf0e89c76f545db4908c6c976901
Shaun B <me@shaunb.ca>
2020-06-24 12:39:54 -0700

    Add Put Summaries Route

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, HTTPException
 from typing import List
 
 from app.api import crud
-from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
+from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema, SummaryUpdatePayloadSchema
 from app.models.tortoise import SummarySchema
 
 
@@ -41,5 +41,14 @@ async def delete_summary(id: int) -> SummaryResponseSchema:
         raise HTTPException(status_code=404, detail="Summary not found")
 
     await crud.delete(id)
+
+    return summary
+
+
+@router.put("/{id}/", response_model=SummarySchema)
+async def update_summary(id: int, payload: SummaryUpdatePayloadSchema) -> SummarySchema:
+    summary = await crud.put(id, payload)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
 
     return summary
--- project/app/models/pydantic.py
+++ project/app/models/pydantic.py
@@ -10,3 +10,7 @@ class SummaryPayloadSchema(BaseModel):
 
 class SummaryResponseSchema(SummaryPayloadSchema):
     id: int
+
+
+class SummaryUpdatePayloadSchema(SummaryPayloadSchema):
+    summary: str
