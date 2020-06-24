28c0ab84dde877bb4965d6b93fdf83c4d52d886c
Shaun B <me@shaunb.ca>
2020-06-24 14:44:46 -0700

    Add text summarizer

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,7 +1,8 @@
 # project/app/api/summaries.py
 
 
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, BackgroundTasks
+
 from typing import List
 
 from app.api import crud
@@ -11,14 +12,17 @@ from app.models.pydantic import (
     SummaryUpdatePayloadSchema,
 )
 from app.models.tortoise import SummarySchema
+from app.summarizer import generate_summary
 
 
 router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+async def create_summary(payload: SummaryPayloadSchema, background_tasks: BackgroundTasks) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
+
+    background_tasks.add_task(generate_summary, summary_id, payload.url)
 
     response_object = {"id": summary_id, "url": payload.url}
     return response_object
