01c38a35ef2110883b70536b2214c9356e0ff4c9
Shaun B <me@shaunb.ca>
2020-06-24 13:05:48 -0700

    Update Put Route Tests: Ensure valid URL

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -187,3 +187,10 @@ def test_update_summary_invalid_keys(test_app_with_db):
             }
         ]
     }
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
+    )
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
