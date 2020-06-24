4cb73332de1bc7dd01b53c01cd20b72574a2c85a
Shaun B <me@shaunb.ca>
2020-06-24 11:53:36 -0700

    Correct formatting with Black

--- project/app/main.py
+++ project/app/main.py
@@ -1,4 +1,3 @@
-
 # project/app/main.py
 
 import logging
@@ -15,7 +14,9 @@ log = logging.getLogger(__name__)
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
-    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
 
     return application
 
--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -5,7 +5,9 @@ import json
 
 
 def test_create_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
@@ -19,14 +21,16 @@ def test_create_summaries_invalid_json(test_app):
             {
                 "loc": ["body", "payload", "url"],
                 "msg": "field required",
-                "type": "value_error.missing"
+                "type": "value_error.missing",
             }
         ]
     }
 
 
 def test_read_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get(f"/summaries/{summary_id}/")
@@ -46,7 +50,9 @@ def test_read_summary_incorrect_id(test_app_with_db):
 
 
 def test_read_all_summaries(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get("/summaries/")
